58d4689c249e771c8bcc8b5fd1eb87b4242a1ae2
Mikhail Stankevich <mikhailstn1@gmail.com>
2023-06-06 09:18:48 +0300

    feat: add video slider

--- src/components/videoJourney/videoSlider.tsx
+++ src/components/videoJourney/videoSlider.tsx
@@ -1,4 +1,5 @@
-import { videos } from '../../data/video';
+import { videos } from "../../data/video";
+import icon from "../../../public/icons/monalisa.png";
 
 export function VideoSlider() {
   return (
@@ -8,9 +9,19 @@ export function VideoSlider() {
           backgroundImage: `url('${el.poster}')`,
         };
         return (
-          <div className="video__slider-item" style={backroundImage}>
-            <h3></h3>
-            <button></button>
+          <div
+            className="video__slider-item"
+            style={backroundImage}
+            key={el.id}
+          >
+            <div className="video__slider-text">
+              <img
+                className="video__slider-item-icon"
+                src={icon}
+              ></img>
+              <h3 className="video__slider-item-title">{el.title}</h3>
+            </div>
+            <button className="video__slider-item-button"></button>
           </div>
         );
       })}
