c8f9173d55fbcc7a3c279b57b5fd1a55bedfe27c
Mikhail Stankevich <mikhailstn1@gmail.com>
2023-08-09 01:56:22 +0300

    feat: add header

---
+++ src/components/buyTicketForm/buyTicketForm.tsx
@@ -0,0 +1,6 @@
+export function BuyTicketForm() {
+  return <form>
+    <div></div>
+    <div></div>
+  </form>;
+}
--- src/pages/buyTicketPage/buyTicketPage.tsx
+++ src/pages/buyTicketPage/buyTicketPage.tsx
@@ -1,18 +1,29 @@
+import "./buyTicketPage.css";
 import { useNavigate } from "react-router-dom";
+import { BuyTicketForm } from "../../components/buyTicketForm/buyTicketForm";
+import logo from "../../../public/icons/logo_gold.png";
 
 export function BuyTicketPage() {
   const navigate = useNavigate();
   return (
     <div className="wrapper">
-      <h1>Buy Ticket</h1>
-      <button
-        onClick={(event) => {
-          event.preventDefault();
-          navigate(-1);
-        }}
-      >
-        ← Back
-      </button>
+      <header className="buy-ticket__header">
+        <button
+          className="buy-ticket__header-btn"
+          onClick={(e) => {
+            e.preventDefault();
+            navigate(-1);
+          }}
+        >
+          ← Back
+        </button>
+        <div className="buy-ticket__logo">
+          <img className="buy-ticket__logo-image" src={logo} alt="logo"></img>
+          <h1 className="buy-ticket__logo__title">booking tickets</h1>
+        </div>
+        <p className="buy-ticket__logo__subtitle">Tour to Louvre</p>
+      </header>
+      <BuyTicketForm />
     </div>
   );
 }
